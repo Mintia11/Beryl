bc2cc39389e1e7149007dea5306d947ce0e10a4a
Mintia11 <129310461+Mintia11@users.noreply.github.com>
2024-09-28 13:50:51 +0200

    Ke: Check limine base revision

--- beryl/src/ke/init.c
+++ beryl/src/ke/init.c
@@ -7,6 +7,9 @@ KiSystemStartup(void)
 {
     printf("Hello, %s\n", "World!");
 
+    uint64_t *base_request = (uint64_t *)KiGetLimineRequest(LIMINE_BASE_REVISION);
+    assert2(base_request[2] == 0, "Boootloader revision 2 is not supported (expected 0 found %d)", base_request[2]);
+
     while (true)
     {
         KeWaitForNextInterrupt();
